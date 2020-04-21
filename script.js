8cd0c58aaaca8cd18e5cf896d17fc506505c3e3c
rodgersea <elliottarodgers@gmail.com>
2020-04-21 19:17:35 -0400

    added electric api to master

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var platipus = "technically a mammal";
-var locations = [1,2,3,4,5,6,7,8,9,10]
+var locations = [];
+var charger_Type = [];
 
 
 $("#submit").click(function(){
@@ -11,29 +12,69 @@ $("#submit").click(function(){
 function renderCards(){
     if($("#fuelType")[0].value == "electric"){
         console.log("electric was selected")
-        // input ajax call that puts location data into location array with objects
-        // input ajax call that puts location data into location array with objects
-        // ajax open
-        // for loop
-        charger = "XY 4000 extreme"
-        address = "42 walaby way, sydney"
-        // ajax close
-        for(var i = 0; i < locations.length; i++){
-            $("#card"+(i+1)).empty()
-            $("#card"+(i+1)).append(`
-                <div class="card">
-                    <div class="card-image">
-                        <img src="images/electric.jpg">
-                        <span class="card-title black opacity">Location Title</span>
-                        <a id="save" class="btn-floating halfway-fab waves-effect waves-light teal"><i class="material-icons">add</i></a>
-                    </div>
-                    <div class="card-content">
-                        <p class="location"><i class="small material-icons">place</i>-${address}-</p>
-                        <p><i class="small material-icons">ev_station</i>Type: ${charger}</p>
-                    </div>
-                </div>
-            `) 
+        // set test variables
+        locations = [];
+        chargerType = [];
+        var lat = "";
+        var lon = "";
+        var max = 10;
+        var e_QueryURL = "";
+        var key = "bb020c16-6b05-47d8-bec0-0e655caac430";
+        var token = "1beabf666d8a29";
+        var address = $("#address").val();
+        console.log(address);
+        var geocode_Base = `https://us1.locationiq.com/v1/search.php?key=${token}&q=${address}&format=json`;
+
+        getLatLon();
+
+        // convert address to lat lon
+        function getLatLon() {
+            $.ajax({
+                url: geocode_Base,
+                method: "GET"
+            }).then(updateLatLon);
         }
+        // set lat and lon variables from api
+        function updateLatLon(latlon) {
+            lat = latlon[0].lat;
+            lon = latlon[0].lon;
+            getURL();
+        }
+        // get electric data from api
+        function getURL(){
+            baseURL = "https://api.openchargemap.io/v3/poi/?";
+            e_QueryURL = `${baseURL}&key=${key}&output=json&latitude=${lat}&longitude=${lon}&maxresults=${max}&distanceunit=Miles&distance=25`
+            $.ajax({        
+                url: e_QueryURL,
+                method: "GET"
+            }).then(updatePage);
+        }
+        // return locations and types of chargers
+        function updatePage(data) {
+            console.log(data);
+            for (i=0; i < data.length; i++) {
+                locations[i] = `Address: ${data[i].AddressInfo.Title} ${data[i].AddressInfo.Town}, ${data[i].AddressInfo.Postcode}`;
+                chargerType[i] = `Charger Type: ${data[i].Connections[0].ConnectionType.FormalName}`;
+            }
+            for(var i = 0; i < locations.length; i++){
+                $("#card"+(i+1)).empty()
+                $("#card"+(i+1)).append(`
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="images/electric.jpg">
+                            <span class="card-title black opacity">Location Title</span>
+                            <a id="save" class="btn-floating halfway-fab waves-effect waves-light teal"><i class="material-icons">add</i></a>
+                        </div>
+                        <div class="card-content">
+                            <p class="location"><i class="small material-icons">place</i>-${locations[i]}-</p>
+                            <p><i class="small material-icons">ev_station</i>Type: ${chargerType[i]}</p>
+                        </div>
+                    </div>
+                `) 
+            }
+        }
+
+
     }else if($("#fuelType")[0].value == "gas"){
         console.log("gas was selected")
         // input ajax call that puts location data into location array with objects

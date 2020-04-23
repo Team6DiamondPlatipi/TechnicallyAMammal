419ef64dd2fd5218e3e5fefdc5a17331c88aa41e
TheGreatWormFromBeyond <58886725+apollodragon13@users.noreply.github.com>
2020-04-23 18:03:44 -0400

    trying again to commit same changes

--- script.js
+++ script.js
@@ -190,7 +190,7 @@ else{
 //         // var savedPlace = $(".savePlace").val();
 //         // savedItems.push(savedPlace);
 //         // console.log(savedItems);
-//         // // localStorage.setItem(locations, value);
+//       localStorage.setItem(locations, value);
 
-//         // // $("#placesList").append(listItem);
+//         $("#placesList").append(listItem);
 // }
